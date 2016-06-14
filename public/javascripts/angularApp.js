4b1ab46faabdf10909721a000a8f575b693834a2
srivastavarnav <srivastavarnav@gmail.com>
2016-06-14 23:32:15 +0530

    getting, posting a post and upvoting a post

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -3,11 +3,26 @@ var app = angular.module('flapperNews',['ui.router']);
 app.factory('posts',['$http',function($http){
 
 	var o = {
-		posts: [{title: 'post 1', upvotes: 5},
-			{title: 'post 2', upvotes: 2},
-			{title: 'post 3', upvotes: 12},
-			{title: 'post 4', upvotes: 22},
-			{title: 'post 5', upvotes: 1}]
+		posts: []
+	};
+
+	o.getAll = function() {
+			return $http.get('/posts').success(function(data){
+				angular.copy(data, o.posts);
+			});
+		};
+
+	o.create = function(post){
+		return $http.post('/posts',post).success(function(data){
+			o.posts.push(data);
+		});
+	};
+
+	o.upvote = function(post){
+		return $http.put('/posts/' + post._id +'/upvote')
+		.success(function(data){
+			post.upvotes += 1;
+		});
 	};
 
 	return o;
@@ -22,7 +37,12 @@ app.config([
 		.state('home',{
 			url:'/home',
 			templateUrl: '/home.html',
-			controller: 'MainCtrl'
+			controller: 'MainCtrl',
+			resolve: {
+				postPromise: ['posts', function(posts){
+					return posts.getAll();
+				}]
+			}
 		})
 		.state('posts',{
 			url: '/posts/{id}',
@@ -51,11 +71,6 @@ app.controller('PostsCtrl', [
 		$scope.incrementUpvotes = function(comment){
 			comment.upvotes += 1;
 		};
-		o.getAll = function() {
-			return $http.get('/posts').success(function(data){
-				angular.copy(data, o.posts);
-			});
-		};
 		
 	}]);
 
@@ -71,22 +86,18 @@ app.controller('MainCtrl', [
 		if(!$scope.title || $scope.title ===''){
 			return;
 		}
-			$scope.posts.push({
+		posts.create({
 			title: $scope.title,
 			link: $scope.link,
-			upvotes: 0,
-			comments: [
-			{author: 'Joe', body: 'Cool post!', upvotes: 0},
-			{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-			]
 		});
+
 		$scope.title = '';
 		$scope.link = '';
 	};
 
 	$scope.incrementUpvotes = function(post){
 
-		post.upvotes +=1;
+		posts.upvote(post);
 	};
 
 }]);
